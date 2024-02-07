560b5eac9bd2c7b2254f289877f36a0a0ed43791
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-02-07 15:30:01 +0800

    2/7/2024 W 3:28p - ATM PIN login UI finished

--- app/src/main/java/com/example/atmsimulator/MainActivity.kt
+++ app/src/main/java/com/example/atmsimulator/MainActivity.kt
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    object Constants {
+        // Tries
+        const val TOTAL_TRIES_ALLOWED = 4
+
+        // Valid pin
+        const val VALID_PIN_1 = "1234"
+        const val VALID_PIN_2 = "5678"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
